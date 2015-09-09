ca3361807667eaf71204870fb89f185aa5104f14
Brandon Giles <10732438@uvu.edu>
2015-09-09 15:28:54 -0600

    no message

---
+++ Variables and Functions/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour 
+{   
+	int MyInt = 5;
+	void Start(){
+		Debug.Log (MyInt);
+		MyInt = MultiplyByTwo (MyInt);
+		Debug.Log (MyInt);
+
+	}
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+}
