31de108a104736ab6fcbd0d4f4efda859fcc116e
Brandon Giles <10732438@uvu.edu>
2015-10-07 15:45:33 -0600

    no message

---
+++ Classes/Assets/Classes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Classes : MonoBehaviour 
+{
+
+	public class Health
+	{
+		public int maxHP;
+		public int currentHP;
+		public int minimumHP;
+
+		public Health (int maxHP, int currentHP)
+	
+		{
+
+
+	
+			maxHP = 100;
+			currentHP = maxHP;
+			currentHP = 58;
+			minimumHP = 20;
+			
+		}
+		public Health characterHealth = new Health (150);
+		public Health followerHealth = new Health (12);
+	}
+	void Start () 
+	{
+		Debug.Log(characterHealth.currentHP);
+
+	}
+}
