7198772b284d9b2ec624c3e73adce4eb96ccf8e3
Brandon Giles <10732438@uvu.edu>
2015-10-26 14:32:24 -0600

    no message

---
+++ Arrays/Assets/Arrays.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrays : MonoBehaviour 
+{
+	public int[]MyArray = {21, 12, 21};
+
+	void Start ()
+	{
+		for(int i = 0; i < MyArray.Length i++) 
+
---
+++ Classes/Assets/Classes1.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Classes1 : MonoBehaviour 
+{
+	public Health
+	{
+		public int Health
+		{
+			public Health (int MaxHP);
+		}
+		{
+			BaseHealth = MaxHP;
+		}
+		public Health ()
+		{
+			BaseHealth = 100;
+		}
+	}
+	public Health MyHealth = new Health (150)
+		Void Start()
+	{
+Debug.Log (MyHealth.BaseHealth)
+}
+}
