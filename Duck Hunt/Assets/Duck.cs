f97321e47062f69caf60aa89baddaf64262caeba
Brandon Giles <10732438@uvu.edu>
2015-11-18 14:48:36 -0700

    no message

---
+++ Duck Hunt/Assets/Duck.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Duck : MonoBehaviour 
+{
+	public void OnTiggerEnter(Collider hit)
+	{
+		print ("test");
+		if(hit.tag == "KillZone")
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+}
