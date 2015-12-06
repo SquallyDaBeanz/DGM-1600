8fe57683eab97217f1907e72b0d3fb32113626dd
SquallyDaBeanz <SquallyDaBeanz@hotmail.com>
2015-12-06 16:30:05 -0700

    no message

--- Duck Hunt/Assets/DuckMovement.cs
+++ Duck Hunt/Assets/DuckMovement.cs
@@ -24,7 +24,7 @@ public class DuckMovement : MonoBehaviour
 		direction = new Vector3(Random.Range (-1f, 1f), Random.Range (.2f, -1f), 0f);
 	}
 
-	public void DirectionChange(Vector3 _dir)
+	public void DirectionChanger(Vector3 _dir)
 	{
 		direction = new Vector3 (direction.x * _dir.x, direction.y * _dir.y, 0);
 	}
