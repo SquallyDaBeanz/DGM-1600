1cf2e6940570546b28b035560201dc1f470cb8f9
SquallyDaBeanz <SquallyDaBeanz@hotmail.com>
2015-12-06 18:20:15 -0700

    no message

---
+++ Duck Hunt/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	
+	public delegate void DuckDel();
+	public static DuckDel OnSpawnDucks;
+	public static DuckDel OnDuckShot;
+	public static DuckDel OnDuckDeath;
+	public static DuckDel OnDuckFlyAway;
+	public static DuckDel OnDuckMiss;
+}
+
---
+++ Duck Hunt/Assets/Shooter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour 
+{
+
+	RaycastHit hit;
+
+	private int bulletAmt;
+	public int maxBullets;
+
+	void start()
+	{
+
+	}
+
+	void update()
+	
+	{
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			bulletAmt--;
+
+			if(bulletAmt <= 0)
+			{
+				GameManager.OnDuckMiss();
+			}
+
+			Vector3 mousePos = Input.mousePosition;
+			mousePos.z = Camera.main.transform.position.z;
+
+			if(Physics.Raycast(Camera.main.ScreenToWorldPoint(mousePos), Camera.main.transform.forward, out hit, Mathf.Infinity))
+			{
+				if(hit.transform.tag == "Duck")
+				{
+					//use getcomponent in hit to get the duck health script. (script that deletes the duck) Killzone? or just "Duck" script. Using that, call the killduck in duck health script. For the KillDuck function.
+				}
+			}
+		}
+	}
+}
